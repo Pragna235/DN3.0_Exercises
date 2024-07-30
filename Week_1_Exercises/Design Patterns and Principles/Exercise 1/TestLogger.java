70710e5830766a9c815423153f260f180eea3999
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-07-30 13:22:29 +0530

    Create TestLogger.java

---
+++ Week_1_Exercises/Design Patterns and Principles/Exercise 1/TestLogger.java
@@ -0,0 +1,21 @@
+package com.singletonpatternexample;
+
+public class TestLogger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger logger1 = Logger.getInstance();
+		Logger logger2 = Logger.getInstance();
+		
+		logger1.log("This is the first log message");
+		logger2.log("This is the second log message");
+
+		if(logger1 == logger2) {
+			System.out.println("Both logger1 and logger2 are the same isntance.");
+		}
+		else {
+			System.out.println("logger1 and logger2 are different isntances.");
+		}
+	}
+
+}
