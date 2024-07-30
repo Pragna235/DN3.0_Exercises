b57b4807b26302f90d064726b127d9ce35564a97
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-07-30 14:17:22 +0530

    Add files via upload

---
+++ Week_1_Exercises/Design Patterns and Principles/FactoryMethodPatternExample/src/com/factorymethodpatternexample/ExcelDocument.java
@@ -0,0 +1,13 @@
+package com.factorymethodpatternexample;
+
+public class ExcelDocument implements Document{
+	@Override
+	public void open() {
+		System.out.println("Opening Excel Document...");
+	}
+	@Override
+	public void close() {
+		System.out.println("Closing Excel Document...");
+	}
+
+}
---
+++ Week_1_Exercises/Design Patterns and Principles/FactoryMethodPatternExample/src/com/factorymethodpatternexample/WordDocumentFactory.java
@@ -0,0 +1,9 @@
+package com.factorymethodpatternexample;
+
+public class WordDocumentFactory extends DocumentFactory{
+	@Override
+	public Document createDocument() {
+		return new WordDocument();
+	}
+
+}
