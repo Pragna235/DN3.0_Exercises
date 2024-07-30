222eb2fe691cee06bdd245018bbf883a0c9b7925
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-07-30 14:16:38 +0530

    Add files via upload

---
+++ Week_1_Exercises/Design Patterns and Principles/SingletonPatternExample/src/com/singletonpatternexample/Logger.java
@@ -0,0 +1,20 @@
+package com.singletonpatternexample;
+
+public class Logger {
+	private static Logger instance;
+	
+	private Logger() {
+		
+	}
+	
+	public static Logger getInstance() {
+		if(instance == null) {
+			instance = new Logger();
+		}
+		return instance;
+	}
+	
+	public void log(String message) {
+		System.out.println("Log : " + message);
+	}
+}
