3ab2ba80f779b242dd4be57d9c8943dce0fe99e5
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-07-30 13:44:26 +0530

    Create PdfDocumentFactory.java

---
+++ Week_1_Exercises/Design Patterns and Principles/Exercise 2/PdfDocumentFactory.java
@@ -0,0 +1,9 @@
+package com.factorymethodpatternexample;
+
+public class PdfDocumentFactory extends DocumentFactory{
+	@Override
+	public Document createDocument() {
+		return new PdfDocument();
+	}
+
+}
